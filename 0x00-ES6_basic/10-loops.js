7f26abc23df935a6b11e49e77b6d983e1e578b13
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-05-27 17:57:29 +0100

    eslint lint updated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,12 +1,12 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (let value of array) {
-      newArray.push(appendString + value);
-    }
-
-    // for (let [idx, value] of array.entries()) {
-    //   array[idx] = appendString + value;
-    // }
-  
-    return newArray;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+
+  // for (let [idx, value] of array.entries()) {
+  //   array[idx] = appendString + value;
+  // }
+
+  return newArray;
+}
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,11 @@
 export default function createReportObject(employeesList) {
-    const report = {
+  const report = {
 
-        allEmployees: employeesList,
-        
-        getNumberOfDepartments(employees) {
-            return Object.keys(employees).length;
-        }
-    }
-    return report;
+    allEmployees: employeesList,
+
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
+  return report;
 }
