f63265cce04e581be25e682207896555dba88bc3
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-05-29 22:17:40 +0100

    task 9 created

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  let value;
+  const queue = [];
+
+  try {
+    value = mathFunction();
+  } catch (err) {
+    value = err.toString();
+  }
+  queue.push(value);
+  queue.push('Guardrail was processed');
+}
