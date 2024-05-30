ce5f1d42d969c62c0413fbbbe4763a886add559c
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-05-30 21:19:42 +0100

    task 6 uploaded

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,7 +1,20 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor() {
-        super()
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
     }
+
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
