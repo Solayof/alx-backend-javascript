c591581bae7c2ad2d481be0ddada29eab4376a4e
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-05-30 19:42:35 +0100

    task 5 code uploaded

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,17 @@
 export default class Building {
-    constructor() {
-        
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqrt must be a number');
     }
+
+    if (this.evacuationWarningMessage === undefined && this.constructor !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqrt = sqft;
+  }
+
+  get sqrt() {
+    return this._sqrt;
+  }
 }
