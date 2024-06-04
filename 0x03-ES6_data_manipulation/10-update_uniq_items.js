d3d1229327b481eb199f960bc5abb7577580f20c
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-06-04 15:19:02 +0100

    task 10 updated

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const kval of map.keys()) {
     if (map.get(kval) === 1) {
       map.set(kval, 100);
