b15d7b984fa286d846db28271cdc2f665fcb08d8
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-06-04 14:35:59 +0100

    task 9 updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString) {
     return '';
   }
-  let str = [];
+  const str = [];
   for (const val of set) {
     if (val.startsWith(startString)) {
       str.push(val.slice(startString.length));
