871f8121c7df67c9d3d5b0404b810552ae699cb3
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-06-05 05:31:20 +0100

    task 8 updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,25 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
   if (!(set instanceof Set)) throw new TypeError('set must be a Set');
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-').toString();
+
+  const strings = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      strings.push(value.slice(startString.length));
+    }
+  }
+
+  return strings.join('-').toString();
 }
+
+// export default function cleanSet(set, startString) {
+//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+//   if (!(set instanceof Set)) throw new TypeError('set must be a Set');
+//   return [...set]
+//     .filter((el) => el.startsWith(startString))
+//     .map((el) => el.slice(startString.length))
+//     .join('-').toString();
+// }
 
 // export default function cleanSet(set, startString) {
 //   if (!startString || typeof startString !== 'string' || typeof set !== 'object') {
