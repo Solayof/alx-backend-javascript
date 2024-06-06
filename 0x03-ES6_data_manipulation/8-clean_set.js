6e9adf108a313b523c5c9b2fa674fe93811b6af7
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-06-06 18:48:52 +0100

    task 8 updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  if (!(set instanceof Set)) throw new TypeError('set must be a Set');
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-').toString();
+  if (!startString || typeof startString !== 'string' || typeof set !== 'object') {
+    return '';
+  }
+  const str = [];
+  for (const val of set) {
+    if (val) {
+      if (val.startsWith(startString)) {
+        str.push(val.slice(startString.length));
+      }
+    }
+  }
+  return str.join('-');
 }
-
-// export default function cleanSet(set, startString) {
-//   if (!startString || typeof startString !== 'string' || typeof set !== 'object') {
-//     return '';
-//   }
-//   const str = [];
-//   for (const val of set) {
-//     if (val.startsWith(startString)) {
-//       str.push(val.slice(startString.length));
-//     }
-//   }
-//   return str.join('-');
-// }
