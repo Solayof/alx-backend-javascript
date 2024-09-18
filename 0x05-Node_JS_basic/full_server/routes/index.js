04e5ae0f1768b05b3b63e44ce262b049580425b1
Moses Solomon Ayofemi <101998774+Solayof@users.noreply.github.com>
2024-09-18 22:36:36 +0100

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -8,5 +8,4 @@ router.get('/', (req, res) => AppController.getHomepage(req, res));
 router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
 router.get('/students/:major', (req, res) => StudentsController.getAllStudentsByMajor(req, res));
 
-export default router;
 module.exports = router;
